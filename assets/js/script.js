a3d3970fdf521635483a6133258ef190f8aeb520
Efraim A. Lorenzana <ef_fame69@yahoo.com>
2018-07-05 21:32:58 +0800

    Update : About us page completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function () {
-	/* Toggle Modal #disclaimer*/
+	// Toggle Modal #disclaimer
 	$('#disclaimer').modal('show');
+
+	// JS for content editable trick from Chris Coyier
+
+	var h1 = document.querySelector("h1");
+
+	h1.addEventListener("input", function() {
+		this.setAttribute("data-heading", this.innerText);
+	});
 });
